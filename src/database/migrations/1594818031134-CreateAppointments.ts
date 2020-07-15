f646cf0fbc4f784fb1cb74f8087b276c28bcb334
w-arantes <w.aranteslfc@gmail.com>
2020-07-15 11:35:22 -0300

    chore: Add TypeORM repository

--- src/database/migrations/1594818031134-CreateAppointments.ts
+++ src/database/migrations/1594818031134-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1594818031134
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
